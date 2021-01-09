b81cf257e50c05da251bf566ba30687c057c5fff
Jurica Lihtar <57017870+juricalihtar@users.noreply.github.com>
2021-01-09 18:06:29 +0100

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,24 @@
+<?php 
+	print '
+	<h1>O nama</h1>
+		<h2>Most Popular Programming Languages 1965 - 2019</h2>
+		<video width="50%" controls poster="img/banner-PHP.jpg" >
+		  <source src="video/Most Popular.mp4" type="video/mp4">
+		  Your browser does not support HTML5 video.
+		</video>
+		<p>Sve tečajeve iz naše ponude možeš pohađati i kroz individualnu nastavu. Prilagodi nastavu svojim potrebama i mnogo brže postigni željeni cilj!</p>
+		<p>Prednosti individualne nastave su:
+			<li a>Brže usvajanje gradiva (u 50% satnice predviđene za grupnu nastavu moguće je prijeći predviđeno gradivo)</li> <br>
+			<li a>Nema ograničenja u broju sati i brzini napredovanja</li> <br>
+			<li a>Potpuna usklađenost sadržaja predavanja s tvojim ciljevima</li> <br>
+			<li a>Maksimalna fleksibilnost termina</li><br>
+			<li a>Mogućnost otkazivanja dogovorenog termina zbog izvanrednih obveza (do 24 sata ranije bez naknade)</li><br>
+			<li a>Mogućnost odabira povoljnijeg plaćanja u paketima od 10, 20, 30 ili više školskih sati</li><br>
+			<li a>Kreirajmo zajedno edukaciju prilagođenu tvojim potrebama</li> <br>
+		<h3>Kako se upisati?</h3>
+		<p>Preporučujemo da nam se javiš i da organiziramo neobavezni susret s predavačem. Nakon što se upoznate, zajedno ćete se dogovoriti o modelu edukacije koji bi najbolje odgovarao tvojem trenutačnom znanju i ciljevima.</p>
+		<p>Naša su ti vrata uvijek otvorena.</p>
+		<p>Napomena: Grupe svih razina formiraju se tijekom cijele godine, zato nikad nije kasno da se uključiš. Za više informacija obrati nam se mailom, telefonom ili nas osobno posjeti.</p>
+		<p>Veselimo se tvom dolasku!</p>
+		<p>Preuzeto sa <a href="https://www.youtube.com/watch?v=Og847HVwRSI" target="_blank">youtube.com</a></p>';
+?>
---
+++ home.php
@@ -0,0 +1,76 @@
+<?php 
+	print '
+	<h1>Become a PROgramer - tečaj programiranja</h1>
+		<figure>
+			<img src="img/coding education.jpg" alt="Tečaj programiranja" title="Tečaj programiranja">
+			<figcaption>Certificirani predavači s višegodišnjim iskustvom.</figcaption>
+		</figure>
+		<h3>Što rade programeri?</h3>
+			<p>Programeri uglavnom piju pivo nakon napornog dana provedenog za računalom. :) Šalimo se.</p>
+
+			<p>Programeri pišu programe (aplikacije) koje koriste drugi ljudi u svrhu olakšanja nekog procesa ili omogućavanja rada na nečem što do tada nije postojalo.</p>
+
+			<h3>Zašto postati programer?</h3>
+			<p>Priča ide otprilike ovako…</p>
+
+			<p>Najbolji scenario bio bi sljedeći. Mladi ste i još uvijek niste odabrali nešto čime ćete se baviti u životu, a pokazujete određenu dozu zanimanja prema računalima.</p>
+
+			<p>Ukoliko ste zaključili kako se želite baviti nečim povezanim s računalima suzili ste izbor gotovo na minimum.</p> <br>
+
+			<p>Smjerovi u kojima dalje uglavnom možete krenuti su:</p>
+				
+				<li b>mreže</li> <br>
+				<li b>programiranje</li> <br>
+				<li b>ostalo (npr. servisiranje računala)</li><br>
+				
+				
+			<p>Nećemo reći zašto (ne)biste odabrali mreže, ali reći ćemo zašto biste odabrali programiranje.</p>
+
+			<p>Ukoliko pogledamo više programera i podvučemo crtu, zaključiti ćemo kako programer:</p>
+			<li>voli stvarati</li> <br>
+			<li>voli matematiku i računanje</li> <br>
+			<li>voli znati kako stvari rade</li> <br>
+			<li>može dugo sjediti</li> <br>
+			<li>zna biti čudan :)</li> <br>
+
+
+			 
+			<p>Svaki programer koji zna programirati ima posao. Nebitno je radi li u nekoj firmi, “u fušu” ili oboje. Ne treba očekivati kako će prvi posao pod nazivom “programer” biti sjajno plaćen, ali s vremenom (iskustvom i referencama) imati ćete dovoljno novaca za ugodan život.</p>
+
+			<h3>Kako postati programer?</h3>
+			<p>Obrazovanje</p>
+			<p>Kada smo zaključili kako je sjajno biti programer, odaberimo školu koja će nas uvesti u svijet programiranja.</p>
+
+			<p>U Hrvatskoj postoji mnogo tehničkih škola sa smjerovima “Tehničar za računalstvo” ili nešto u tom stilu. Predlažemo da upišete takvu školu jer ćete između svih ostalih “nebitnih stvari” naučiti i ponešto o programiranju. Naravno, to neće biti dovoljno kako biste se mogli nazvati programerom, ali dovoljno da odlučite hoćete li se baviti time u životu.</p>
+
+			<p>Dok ste još u srednjoj školi, bitno je da osim u školi pokušavate programirati dok vaši vršnjaci uživaju u slobodnom vremenu.</p>
+
+			<p>Nakon što potvrdite kako je programiranje super, slijedi odabir fakulteta. Naravno, ne treba ići na fakultet kako biste naučili programirati, ali ćete spoznati smjernice i steći neko znanje koje ćete opet u slobodno vrijeme nadograđivati. Bilo koji fakultet koji ima smjer programiranje (programsko inženjerstvo) biti će dovoljan.</p>
+
+			<p>Ukoliko ste stariji, uvijek postoji mogućnost školovanja u učilištima poput Algebre.</p>
+
+			<h3>Kako počinje?</h3>
+			<p>Generalno, svi započinju programiranjem u C-u. Nakon što se usvoji razmišljanje i nakon što se dobije pogled na to, C nije dovoljan. Tada se prelazi na druge programske jezike, obično objektno orijentirane (C++, JAVA, PHP, …). Nakon što se nauči još jedan programski jezik tada postaje svejedno – prelaskom na bilo koji drugi jezik vi ćete znati programirati. Ono što će biti potrebno je naučiti sintaksu (pravila pisanja tog jezika) i prednosti i mane u odnosu na druge programske jezike.</p>
+
+			<p>Postoje i razlike u vrijednosti između programera određenih programskih jezika. Npr. JAVA programeri u pravilu su bolje plaćeni od PHP programera. No, nadamo se kako to neće biti kriterij pri odluci za određeni programski jezik.</p>
+
+
+			 
+			<h3>Prvi posao</h3>
+			<p>Biti će ga teško naći jer svi poslodavci žele iskustvo. Predlažemo da tada pokušate “u fušu” i napravite nekoliko kvalitetnih aplikacija koje biste kasnije prikazivali kao reference.</p>
+
+			<p>Ukoliko imate sreće, naći ćete posao pripravnika na kojem ćete između nošenja kava učiti i programirati. :)</p>
+
+			<h3>Kraj!</h3>
+			<p>Da, radi se o procesu koji traje nekoliko godina i nitko ne kaže kako je lako. Zato, naouružajte se voljom i strpljenjem! ;)</p>
+
+			<p>Na kraju, ovo nije pravilo! Postoje srednjoškolci koji programiraju bolje od ljudi koji su završili fakultet i nazivaju se programerima. Postoje i stariji ljudi koji su igrom slučaja pokušali i naučili programirati, bez da su išli u školu za to, i programiraju bolje od nekih ljudi koji su cijelo školovanje posvetili programiranju.</p>
+
+			<p>Sretno!</p>
+		<p>Izvor: <a href="https://www.ucionica.net/" target="_blank">Učionica</a></p>
+		<p>Društvene mreže:<br>
+			<a href="https://www.linkedin.com" target="_blank"><img src="img/linkedin.svg" alt="Linkedin" title="Linkedin" style="width:24px; margin-top:0.4em"></a>
+			<a href="https://twitter.com" target="_blank"><img src="img/twitter.svg" alt="Twitter" title="Twitter" style="width:24px; margin-top:0.4em"></a>
+			<a href="https://instagram.com" target="_blank"><img src="img/instagram.svg" alt="Instagram" title="Instagram" style="width:24px; margin-top:0.4em"></a>
+		</p>';
+?>
